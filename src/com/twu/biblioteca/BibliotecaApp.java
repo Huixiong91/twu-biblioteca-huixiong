d2fd52bd2f631e132f3014c77175a30469e8f19a
HuiXiongMacBookAir <HuiXiong9191@gmail.com>
2018-04-19 15:59:22 +0800

    Add list books and list book detail

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,69 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
+    List<Book> books = new ArrayList<Book>();
+    public BibliotecaApp() {
+        Book book_1 = new Book("Book 1", "Mr Lee", "2004");
+        Book book_2 = new Book("Book 2", "James Cook", "2011");
+        Book book_3 = new Book("Book 3", "Peter Ang", "2015");
+        books.add(book_1);
+        books.add(book_2);
+        books.add(book_3);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Welcome to Biblioteca-HuiXiong!");
+        BibliotecaApp app = new BibliotecaApp();
+    }
+
+    public void listBook() {
+        System.out.println("List of books available");
+        int index = 1;
+        for (Book book : books){
+            System.out.println(index++ + ": " + book.title);
+        }
+    }
+
+    public void printBookDetail(Book b) {
+        System.out.println("Author: " + b.author + " Year Published: " + b.yearPublished);
+    }
+
+    public class Book {
+        String title;
+        String author;
+        String yearPublished;
+
+        public Book(String title, String author, String yearPublished) {
+            this.title = title;
+            this.author = author;
+            this.yearPublished = yearPublished;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getYearPublished() {
+            return yearPublished;
+        }
+
+        public void setYearPublished(String yearPublished) {
+            this.yearPublished = yearPublished;
+        }
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,56 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    private BibliotecaApp app = new BibliotecaApp();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+        System.setErr(System.err);
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testMain() {
+        BibliotecaApp.main(null);
+        assertEquals("Welcome to Biblioteca-HuiXiong!\n", outContent.toString());
+    }
+
+    @Test
+    public void testListBook() {
+        app.listBook();
+        String expected = "List of books available\n";
+        int index = 1;
+        for (BibliotecaApp.Book b : app.books) {
+            expected += index++ + ": " + b.title + "\n";
+        }
+        assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    public void testPrintBookDetail(){
+        BibliotecaApp.Book b = app.books.get(1);
+        app.printBookDetail(app.books.get(1));
+        assertEquals("Author: " + b.author + " Year Published: " + b.yearPublished +"\n", outContent.toString());
+
     }
 }
